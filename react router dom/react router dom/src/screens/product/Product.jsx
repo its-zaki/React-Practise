5d49abf0c8f67abe41a326c0279549b946577ca1
its zaki <zakiyounus789@gmail.com>
2024-01-12 13:39:53 +0500

    react practise

--- react router dom/react router dom/src/screens/product/Product.jsx
+++ react router dom/react router dom/src/screens/product/Product.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import {useParams} from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
 const Product = () => {
     const params = useParams()
     console.log(params);
+    useEffect(()=>{
+      axios.get("https://fakestoreapi.com/products").then((res)=>{
+        console.log(res);
+      }).catch((err)=>{
+        console.log(err);
+      })
+
+    })
+
   return (
+    <>
     <div>Product{params.id}</div>
+    </>
+
   )
 }
 
